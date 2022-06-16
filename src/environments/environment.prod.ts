0cd7fa32854b51d3691e38e9f826add7e137735a
DJ Kullas <dkoolj5@gmail.com>
2022-06-16 15:37:56 -0400

    this stuff is public

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'augie-s-food-truck',
+    appId: '1:704629781825:web:7ca4f15f9c6d5f54595dc0',
+    storageBucket: 'augie-s-food-truck.appspot.com',
+    apiKey: 'AIzaSyCTdx1z7MGEJwdQZPxYByiFftfA5YNl-Kc',
+    authDomain: 'augie-s-food-truck.firebaseapp.com',
+    messagingSenderId: '704629781825',
+    measurementId: 'G-Q2M3S40BN8',
+  },
+  production: true,
+  url: ''
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  firebase: {
+    projectId: 'augie-s-food-truck',
+    appId: '1:704629781825:web:7ca4f15f9c6d5f54595dc0',
+    storageBucket: 'augie-s-food-truck.appspot.com',
+    apiKey: 'AIzaSyCTdx1z7MGEJwdQZPxYByiFftfA5YNl-Kc',
+    authDomain: 'augie-s-food-truck.firebaseapp.com',
+    messagingSenderId: '704629781825',
+    measurementId: 'G-Q2M3S40BN8',
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyCTdx1z7MGEJwdQZPxYByiFftfA5YNl-Kc",
+    authDomain: "augie-s-food-truck.firebaseapp.com",
+    projectId: "augie-s-food-truck",
+    storageBucket: "augie-s-food-truck.appspot.com",
+    messagingSenderId: "704629781825",
+    appId: "1:704629781825:web:7ca4f15f9c6d5f54595dc0",
+    measurementId: "G-Q2M3S40BN8"
+  },
+  production: false,
+  url: ''
+  };
